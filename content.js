1617c9c026717b5986068f0c273db2b707785c90
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2023-08-23 16:13:31 +0900

    v1.1
    window.onbeforeunload挟んで、window.location.replace()の動作を阻止する事にした。
    window.locationを上書きしてみようとするも、案の定無理だった。
    もう少し模索してみようかなと思った。

--- content.js
+++ content.js
@@ -1,7 +1,19 @@
+if(location.href == "https://tweetdeck.twitter.com/"){
+    function wait(set_num) {
+        var start_time = new Date();
+        while (new Date()-start_time<set_num);
+    };
+    if(confirm("OKを押して画面を数クリックしてください。\r\n「このサイトを離れますか?」の表示でキャンセルをクリックします。\r\n Press OK and click the screen a few clicks.\r\n \"Do you want to leave this site?\" and click Cancel.")){
+        wait(1500);
+    };
+    //window.location.replace()検出
+    window.onbeforeunload = function() {
+        return "「このサイトを離れますか?」の表示でキャンセルをクリックします。\r\n Press OK and click the screen a few clicks.\r\n \"Do you want to leave this site?\" and click Cancel.";
+    };
+}
 chrome.runtime.sendMessage({message: "wake_up"});
 if(location.href == "https://twitter.com/i/premium_sign_up"){
-    if(confirm("TweetDeck(X Pro)へ移動しますか?")){
+    if(confirm("TweetDeck(X Pro)へ移動しますか?\r\nWould you like to move to TweetDeck(X Pro)?")){
         location.href = "https://tweetdeck.twitter.com/";
     }
 }
-
